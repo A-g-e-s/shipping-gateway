14522bbd4d809417849a24ab2aeb4ce44c53657a
Ages <jiri.hovad@outlook.com>
2026-04-30 21:32:39 +0200

    supportedParcelTypes

--- src/Common/Carrier.php
+++ src/Common/Carrier.php
@@ -4,10 +4,21 @@ declare(strict_types=1);
 
 namespace Ages\ShippingGateway\Common;
 
+use Ages\ShippingGateway\Common\Shipment\ParcelType;
+
 enum Carrier: string
 {
     case Gls = 'gls';
     case Ppl = 'ppl';
     case CzechPost = 'czechpost';
     case GebruderWeiss = 'gbw';
+
+    /** @return ParcelType[] */
+    public function supportedParcelTypes(): array
+    {
+        return match ($this) {
+            self::Gls, self::Ppl, self::CzechPost => [ParcelType::Package, ParcelType::PackageOversize],
+            self::GebruderWeiss => ParcelType::cases(),
+        };
+    }
 }
